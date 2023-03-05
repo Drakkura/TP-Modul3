de07584184e80edf6c987077020313de21f2c055
Drakkura <gonhgyhjj1@gmail.com>
2023-03-05 16:49:00 +0700

    Input Output

--- MOD3/Form1.cs
+++ MOD3/Form1.cs
@@ -16,5 +16,15 @@ namespace MOD3
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
